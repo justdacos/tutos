6dbd469255f7e533cb237982d0be48dba2a1159c
Auguste Dacosta <justdacos@gmail.com>
2018-12-14 16:50:18 +0100

    Add production settings

---
+++ acme/prod_settings.py
@@ -0,0 +1,8 @@
+from acme.settings import *
+
+DEBUG = False
+TEMPLATE_DEBUG = False
+
+SECRET_KEY = 'od9l#y3wf!r9&$dyvl@d5da8^_(*=wap4i4!rubj72o&2)7@b!'
+
+ALLOWEB_HOSTS = ['acme-blog.herokuapp.com']
